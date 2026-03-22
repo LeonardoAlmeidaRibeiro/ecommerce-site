1c9d6fc238d7903a2e27f7933123a04160aaeb02
Leonardo <leonardo.a.ribeiro.21@gmail.com>
2026-03-21 22:40:36 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/produtos', [OrderController::class, 'index'])->name('produtos.index');
+Route::get('/', [OrderController::class, 'index'])->name('produtos.index');
